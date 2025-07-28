d8c12a807c0ddaf164eb7e2470893745bccfe2f9
Paulo Henrique Lima <ph.blima30@gmail.com>
2025-07-28 01:16:56 -0300

    fix(rota): corrigir rotas para criação e edição de agendamento standalone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,31 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./pages/agendamentos/agendamentos/agendamentos.component')
-        .then(m => m.AgendamentosComponent)
+    redirectTo: 'agendamentos',
+    pathMatch: 'full'
   },
-  { path: '**', redirectTo: '' }
+  {
+    path: 'agendamentos',
+    loadComponent: () =>
+      import('./pages/agendamentos/listar-agendamentos/listar-agendamentos.component')
+        .then(m => m.ListarAgendamentosComponent)
+  },
+  {
+    path: 'agendamentos/criar',
+    loadComponent: () =>
+      import('./pages/agendamentos/criar-agendamento/criar-agendamento.component')
+        .then(m => m.CriarAgendamentoComponent)
+  },
+  {
+    path: 'agendamentos/editar/:id',
+    loadComponent: () =>
+      import('./pages/agendamentos/editar-agendamento/editar-agendamento.component')
+        .then(m => m.EditarAgendamentoComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'agendamentos'
+  }
 ];
 
 @NgModule({
