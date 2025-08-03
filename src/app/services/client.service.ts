9c82029b416717867dd1191d2da9db729810d522
Paulo Henrique Lima <ph.blima30@gmail.com>
2025-08-03 03:28:04 -0300

    feat: integra listagem de clientes com o backend via ClientService

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,59 @@
+// src/app/services/client.service.ts
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Client {
+  id?: number;
+  name: string;
+  cpf: string;
+  birthDate: string;
+  email: string;
+  phoneNumber: string;
+  appointmentPrice: number;
+  appointmentFrequency: number;
+  treatmentStartDate: string;
+  treatmentEndDate: string;
+  appointmentDurationInMinutes: number;
+}
+
+export interface ClientMin {
+  id: number;
+  name: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ClientService {
+  private baseUrl = '/api/clients';
+
+  constructor(private http: HttpClient) {}
+
+  getClients(page: number = 0, size: number = 10, nameFilter?: string) {
+    let params = new HttpParams()
+      .set('page', page)
+      .set('size', size);
+
+    if (nameFilter) {
+      params = params.set('name', nameFilter);
+      return this.http.get<any>(`${this.baseUrl}/findByName`, { params });
+    }
+
+    return this.http.get<any>(this.baseUrl, { params });
+  }
+
+  getClient(id: number): Observable<Client> {
+    return this.http.get<Client>(`${this.baseUrl}/${id}`);
+  }
+
+  createClient(client: Client): Observable<Client> {
+    return this.http.post<Client>(this.baseUrl, client);
+  }
+
+  updateClient(id: number, client: Client): Observable<Client> {
+    return this.http.put<Client>(`${this.baseUrl}/${id}`, client);
+  }
+
+  deleteClient(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
