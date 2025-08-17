0e40a637ecc846feaf1f30738fadb05015753ee3
Paulo Henrique Lima <ph.blima30@gmail.com>
2025-08-17 03:28:32 -0300

    feat(client-form): adicionar e remover contatos de emergência - FormArray no FormularioClienteComponent - Validações e tipagem correta - Estilo diferenciado para contatos de emergência

---
+++ src/app/services/emergency-contact.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface EmergencyContact {
+  id?: number;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  relationship: string;
+  clientId?: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmergencyContactService {
+
+  private baseUrl = 'http://localhost:8080/emergency-contacts'; // ajuste para a sua API
+
+  constructor(private http: HttpClient) { }
+
+  getByClientId(clientId: number): Observable<EmergencyContact[]> {
+    return this.http.get<EmergencyContact[]>(`${this.baseUrl}/client/${clientId}`);
+  }
+
+  create(contact: EmergencyContact): Observable<EmergencyContact> {
+    return this.http.post<EmergencyContact>(this.baseUrl, contact);
+  }
+
+  update(id: number, contact: EmergencyContact): Observable<EmergencyContact> {
+    return this.http.put<EmergencyContact>(`${this.baseUrl}/${id}`, contact);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
